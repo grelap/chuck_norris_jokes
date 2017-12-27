fa014c97766969c4304733f1a2c75e539de22c5c
xenogonx <pablez@wp.pl>
2017-12-27 11:19:04 +0100

    small changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,24 @@
-// var url = 'http://api.icndb.com/jokes/random';
-var url = 'https://api.chucknorris.io/jokes/random';
- 
+var url = 'http://api.icndb.com/jokes/random'
 
-var button = document.getElementById('get-joke'); 
-button.addEventListener('click', function(){ 
-	getJoke(); 
-});
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function () {
+  getJoke()
+})
 
 // ładowanie losowego dowcipu po załadowaniu strony
-document.body.onload = function(){
-	getJoke();
-};
-
+document.body.onload = function () {
+  getJoke()
+}
 
 var paragraph = document.getElementById('joke');
 
-
-function getJoke() { 
-	var xhr = new XMLHttpRequest(); 
-	xhr.open('GET', url); 
-	xhr.addEventListener('load', function(){
-		 var response = JSON.parse(xhr.response); 
-		 paragraph.innerHTML = response.value.joke; 
-		});
-		xhr.send();
-	 }
-
+function getJoke () {
+  var xhr = new XMLHttpRequest(); 
+  xhr.open('GET', url); 
+  xhr.addEventListener('load', function(){
+    var response = JSON.parse(xhr.response); 
+    paragraph.innerHTML = response.value.joke; 
+  })
+  xhr.send()
+}
 var response = JSON.parse(xhr.response);
-
