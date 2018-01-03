e3b6d0b35dda81c324bca09a416983d40166c9c2
xenogonx <pablez@wp.pl>
2018-01-03 07:50:37 +0100

    css-changes

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ var paragraph = document.getElementById('joke');
 
 function getJoke() {
   var xhr = new XMLHttpRequest()
-  xhr.open('GET', url)
+  xhr.open('GET', url);
   xhr.addEventListener('load', function () {
     var response = JSON.parse(xhr.response)
     paragraph.innerHTML = response.value.joke
